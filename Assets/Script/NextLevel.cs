b1f06c4e674a91a3b64a73a93be7416ee52d487b
ItsKreem <itskreeemmm@gmail.com>
2024-04-23 13:04:21 +0800

    LAST LESSON
    added a way to finish a level added damage and ending

--- Assets/Script/NextLevel.cs
+++ Assets/Script/NextLevel.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProgressLevel : MonoBehaviour
+public class ProgressLevel : GameManager
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
+        if (!collision.CompareTag("Player"))
+            return;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GoToNextLevel();
     }
 }
